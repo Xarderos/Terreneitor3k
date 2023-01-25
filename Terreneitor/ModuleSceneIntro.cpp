2aa3a904b828a130103f9769cbafe207e5c732e4
GHOSTVCL <joanetgiolet@gmail.com>
2023-01-25 22:59:27 +0100

    camençament mapes

--- Terreneitor/ModuleSceneIntro.cpp
+++ Terreneitor/ModuleSceneIntro.cpp
@@ -43,12 +43,120 @@ bool ModuleSceneIntro::CleanUp()
 // Update
 update_status ModuleSceneIntro::Update(float dt)
 {
+
+
 	Plane p(0, 1, 0, 0);
 	p.axis = true;
 	p.Render();
+
 	vec3 look = BtToVec(App->player->vehicle->vehicle->getRigidBody()->getCenterOfMassPosition());
 	App->camera->LookAt(look);
+
+	Cube floor(500, 0.5, 500);
+	floor.SetPos(0, -0.25, 0);
+	floor.color.r = 0;
+	floor.color.g = 256;
+	floor.color.b = 0;
+	floor.Render();
+
+	Cube r1(0.5, 1, 155);
+	r1.SetPos(5, 0, -2.5);
+	r1.color.r = 256;
+	r1.color.g = 0;
+	r1.color.b = 0;
+	r1.Render();
+
+	Cube r2(0.5, 1, 140);
+	r2.SetPos(-5, 0, -5);
+	r2.color.r = 256;
+	r2.color.g = 0;
+	r2.color.b = 0;
+	r2.Render();
+
+	Cube r3(30, 1, 0.5);
+	r3.SetPos(-10, 0, 75);
+	r3.color.r = 256;
+	r3.color.g = 0;
+	r3.color.b = 0;
+	r3.Render();
+
+	Cube r4(30, 1, 0.5);
+	r4.SetPos(-20, 0, 65);
+	r4.color.r = 256;
+	r4.color.g = 0;
+	r4.color.b = 0;
+	r4.Render();
+
+	Cube r5(0.5, 1, 30);
+	r5.SetPos(-35, 0, 80);
+	r5.color.r = 256;
+	r5.color.g = 0;
+	r5.color.b = 0;
+	r5.Render();
 	
+	Cube r6(0.5, 1, 30);
+	r6.SetPos(-25, 0, 90);
+	r6.color.r = 256;
+	r6.color.g = 0;
+	r6.color.b = 0;
+	r6.Render();
+
+	Cube r7(50, 1, 0.5);
+	r7.SetPos(-60, 0, 95);
+	r7.color.r = 256;
+	r7.color.g = 0;
+	r7.color.b = 0;
+	r7.Render();
+	
+	Cube r8(70, 1, 0.5);
+	r8.SetPos(-60, 0, 105);
+	r8.color.r = 256;
+	r8.color.g = 0;
+	r8.color.b = 0;
+	r8.Render();
+
+	Cube r9(0.5, 1, 40);
+	r9.SetPos(-85, 0, 75);
+	r9.color.r = 256;
+	r9.color.g = 0;
+	r9.color.b = 0;
+	r9.Render();
+
+	Cube r10(0.5, 1, 60);
+	r10.SetPos(-95, 0, 75);
+	r10.color.r = 256;
+	r10.color.g = 0;
+	r10.color.b = 0;
+	r10.Render();
+
+	Cube r11(5, 1, 0.5);
+	r11.SetPos(-82.5, 0, 55);
+	r11.color.r = 256;
+	r11.color.g = 0;
+	r11.color.b = 0;
+	r11.Render();
+
+	Cube r12(25, 1, 0.5);
+	r12.SetPos(-82.5, 0, 45);
+	r12.color.r = 256;
+	r12.color.g = 0;
+	r12.color.b = 0;
+	r12.Render();
+
+	Cube r13(0.5, 1, 30);
+	r13.SetPos(-80, 0, 70);
+	r13.color.r = 256;
+	r13.color.g = 0;
+	r13.color.b = 0;
+	r13.Render();
+
+	Cube r14(0.5, 1, 30);
+	r14.SetPos(-70, 0, 60);
+	r14.color.r = 256;
+	r14.color.g = 0;
+	r14.color.b = 0;
+	r14.Render();
+
 	return UPDATE_CONTINUE;
 }
 
--- Terreneitor/ModuleSceneIntro.h
+++ Terreneitor/ModuleSceneIntro.h
@@ -41,4 +41,7 @@ public:
 
 	PhysMotor3D* left_wheel;
 	PhysMotor3D* right_wheel;
+
+	PhysBody3D* square;
+	Cube floor_;
 };
